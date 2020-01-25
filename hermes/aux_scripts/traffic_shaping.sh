09b64b997493439b11d5e11be29a3ce8e7b698f9
Electric Ant <electricant@anche.no>
2020-01-25 12:51:08 +0100

    Increase download speed limits and provide a different latency setting for upload packets

--- hermes/aux_scripts/traffic_shaping.sh
+++ hermes/aux_scripts/traffic_shaping.sh
@@ -4,11 +4,12 @@
 # The classes are simple as I'm am intrested to keep interactivity high by
 # limiting the maximum throughput
 
-SPEED_DOWN="24Mbit"
-BURST_DOWN="64kb"
+SPEED_DOWN="25Mbit"
+BURST_DOWN="128kb"
+LATENCY_DOWN="100ms"
 SPEED_UP="18Mbit"
 BURST_UP="16kb"
-LATENCY="100ms"
+LATENCY_UP="75ms"
 
 # see https://linux.die.net/man/8/tc-prio
 PRIOMAP="1 1 1 1 1 1 0 0 1 1 1 1 1 1 1 1"
@@ -23,7 +24,7 @@ tc qdisc del dev eth1 root
 tc qdisc add dev eth0 root handle 1: prio bands 2 priomap $PRIOMAP
 tc qdisc add dev eth0 parent 1:1 handle 10: pfifo_fast
 tc qdisc add dev eth0 parent 1:2 handle 20: tbf rate $SPEED_DOWN \
-	latency $LATENCY burst $BURST_DOWN
+	latency $LATENCY_DOWN burst $BURST_DOWN
 # send packets marked as 10 to the pfifo for low latency
 tc filter add dev eth0 parent 1:0 protocol ip prio 10 handle 10 fw flowid 1:1
 
@@ -32,7 +33,7 @@ tc filter add dev eth0 parent 1:0 protocol ip prio 10 handle 10 fw flowid 1:1
 tc qdisc add dev eth1 root handle 1: prio bands 2 priomap $PRIOMAP
 tc qdisc add dev eth1 parent 1:1 handle 10: pfifo_fast
 tc qdisc add dev eth1 parent 1:2 handle 20: tbf rate $SPEED_UP \
-	latency $LATENCY burst $BURST_UP
+	latency $LATENCY_UP burst $BURST_UP
 # send packets marked as 10 to the pfifo for low latency
 tc filter add dev eth1 parent 1:0 protocol ip prio 10 handle 10 fw flowid 1:1
 
