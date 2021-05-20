3cfec03b66c69109f8d39934b9992f5303c29364
Electric Ant <electricant@anche.no>
2021-05-20 18:45:12 +0200

    Use mktemp  to create temp files and add some more blocklists

--- hermes/aux_scripts/blocklist-update.sh
+++ hermes/aux_scripts/blocklist-update.sh
@@ -6,13 +6,20 @@
 set -e # Exit when any command fails
 
 # Lists to pull the hosts from
+#
+# See also:
+#	https://firebog.net/
+#	https://www.github.developerdan.com/hosts/
 SOURCES=("http://www.malwaredomainlist.com/hostslist/hosts.txt"
          "https://raw.githubusercontent.com/FadeMind/hosts.extras/master/add.Dead/hosts"
 	   "https://raw.githubusercontent.com/FadeMind/hosts.extras/master/add.Spam/hosts"
-	   "https://raw.githubusercontent.com/mitchellkrogza/Badd-Boyz-Hosts/master/hosts"
-	   "https://raw.githubusercontent.com/azet12/KADhosts/master/KADhosts.txt"
 	   "https://someonewhocares.org/hosts/hosts"
+	   "https://raw.githubusercontent.com/PolishFiltersTeam/KADhosts/master/KADhosts.txt"
+	   "https://raw.githubusercontent.com/FadeMind/hosts.extras/master/UncheckyAds/hosts"
+	   "https://raw.githubusercontent.com/FadeMind/hosts.extras/master/add.2o7Net/hosts"
 	   "https://raw.githubusercontent.com/llacb47/mischosts/master/social/tiktok-block"
+	   "https://raw.githubusercontent.com/crazy-max/WindowsSpyBlocker/master/data/hosts/spy.txt"
+#	   "https://hostfiles.frogeye.fr/firstparty-trackers-hosts.txt"
 	   "https://raw.githubusercontent.com/llacb47/mischosts/master/microsoft-telemetry"
 	  )
 
@@ -28,13 +35,16 @@ echo "" > $TEMPFILE
 # Download sources and append them to the temp file
 for src in ${SOURCES[@]}
 do
-	echo Downloading $src
+	partialTmp=$(mktemp).block
+	
+	echo Downloading: $src
 	# remove unneeded whitespace and make all ips 0.0.0.0
-	curl -s $src | sed 's/\r//;s/127.0.0.1/0.0.0.0/' >>$TEMPFILE
+	curl -s $src | sed 's/\r//;s/127.0.0.1/0.0.0.0/' >$partialTmp
+	echo Hosts: $(wc -l $partialTmp)
+	cat $partialTmp >>$TEMPFILE
 done
 
-echo Lines before cleanup:
-wc -l $TEMPFILE
+echo Lines before cleanup: $(wc -l $TEMPFILE)
 
 # Cleanup ip addresses (ignoring comments) & remove localhost stuff
 cat $TEMPFILE | awk '/^[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+/ { print $1,$2 }' \
@@ -42,8 +52,7 @@ cat $TEMPFILE | awk '/^[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+/ { print $1,$2 }' \
 # Cleanup 2nd pass: remove duplicates
 sort -u $TEMPFILE.dedup > $TEMPFILE
 
-echo Lines after cleanup:
-wc -l $TEMPFILE
+echo Lines after cleanup: $(wc -l $TEMPFILE)
 
 # Now convert the list entries into a format that unbound can handle:
 # We want to retrieve the domain part only and tell unbound to always return
