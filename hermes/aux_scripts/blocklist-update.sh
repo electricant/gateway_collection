5398b71a0c39b8ee9e4d7854bba038851d997806
Electric Ant <electricant@anche.no>
2021-10-01 10:47:10 +0200

    Improve bdomain blocklist downloader script

--- hermes/aux_scripts/blocklist-update.sh
+++ hermes/aux_scripts/blocklist-update.sh
@@ -10,17 +10,21 @@ set -e # Exit when any command fails
 # See also:
 #	https://firebog.net/
 #	https://www.github.developerdan.com/hosts/
-SOURCES=("http://www.malwaredomainlist.com/hostslist/hosts.txt"
-         "https://raw.githubusercontent.com/FadeMind/hosts.extras/master/add.Dead/hosts"
+SOURCES=("https://raw.githubusercontent.com/FadeMind/hosts.extras/master/add.Dead/hosts"
 	   "https://raw.githubusercontent.com/FadeMind/hosts.extras/master/add.Spam/hosts"
-	   "https://someonewhocares.org/hosts/hosts"
-	   "https://raw.githubusercontent.com/PolishFiltersTeam/KADhosts/master/KADhosts.txt"
 	   "https://raw.githubusercontent.com/FadeMind/hosts.extras/master/UncheckyAds/hosts"
-	   "https://raw.githubusercontent.com/FadeMind/hosts.extras/master/add.2o7Net/hosts"
+	   "https://raw.githubusercontent.com/FadeMind/hosts.extras/master/add.Risk/hosts"
+	   "https://raw.githubusercontent.com/FadeMind/hosts.extras/master/CoinBlockerList/hosts"
+	   "https://raw.githubusercontent.com/FadeMind/hosts.extras/master/osint.digitalside.it/hosts"
 	   "https://raw.githubusercontent.com/llacb47/mischosts/master/social/tiktok-block"
-	   "https://raw.githubusercontent.com/crazy-max/WindowsSpyBlocker/master/data/hosts/spy.txt"
-#	   "https://hostfiles.frogeye.fr/firstparty-trackers-hosts.txt"
 	   "https://raw.githubusercontent.com/llacb47/mischosts/master/microsoft-telemetry"
+	   "https://raw.githubusercontent.com/crazy-max/WindowsSpyBlocker/master/data/hosts/spy.txt"
+	   "https://raw.githubusercontent.com/DandelionSprout/adfilt/master/Alternate%20versions%20Anti-Malware%20List/AntiMalwareHosts.txt"
+	   "https://someonewhocares.org/hosts/hosts"
+# Too many hostnames!
+	   "https://phishing.army/download/phishing_army_blocklist.txt"
+#	   "https://raw.githubusercontent.com/StevenBlack/hosts/master/hosts"
+#	   "https://hostfiles.frogeye.fr/firstparty-trackers-hosts.txt"
 	  )
 
 # Temporary file where the hosts are stored
@@ -35,31 +39,36 @@ echo "" > $TEMPFILE
 # Download sources and append them to the temp file
 for src in ${SOURCES[@]}
 do
-	partialTmp=$(mktemp).block
+	partialTmp=$(mktemp -t XXXXXX.block.part)
 	
 	echo Downloading: $src
-	# remove unneeded whitespace and make all ips 0.0.0.0
-	curl -s $src | sed 's/\r//;s/127.0.0.1/0.0.0.0/' >$partialTmp
-	echo Hosts: $(wc -l $partialTmp)
-	cat $partialTmp >>$TEMPFILE
+	# Remove ips & whitespaces since we don't need them
+	curl -s $src \
+		| sed -r 's/(\b[0-9]{1,3}\.){3}[0-9]{1,3}\b\s//;s/\s+//' >$partialTmp
+	wc -l $partialTmp
+	# Also remove comments & DOS CR from lines
+	cat $partialTmp | sed 's/#.*$//g;s/\r$//g' >>$TEMPFILE
 done
 
 echo Lines before cleanup: $(wc -l $TEMPFILE)
 
-# Cleanup ip addresses (ignoring comments) & remove localhost stuff
-cat $TEMPFILE | awk '/^[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+/ { print $1,$2 }' \
-     | grep -vE 'local$|localhost.*$|broadcasthost$|ip6-.*$' > $TEMPFILE.dedup
-# Cleanup 2nd pass: remove duplicates
+# Cleanup: remove localhost stuff, invalid and duplicate domains
+cat $TEMPFILE \
+	| grep -vE 'local$|localhost.*$|broadcasthost$|ip6-.*$' \
+	| grep -E '^(([[:alnum:]_-]([[:alnum:]_-])*)\.)+[a-zA-Z]{2,}$' \
+	>$TEMPFILE.dedup
 sort -u $TEMPFILE.dedup > $TEMPFILE
 
 echo Lines after cleanup: $(wc -l $TEMPFILE)
 
-# Now convert the list entries into a format that unbound can handle:
-# We want to retrieve the domain part only and tell unbound to always return
-# NXDOMAIN for such a request. This is faster and requires less memory.
+# Now convert the list entries into a format that unbound can handle, ignoring
+# empty lines. We want to tell unbound to always return NXDOMAIN for such 
+# domain requests and for all subdomains. This is faster and requires less
+# memory.
 cat $TEMPFILE \
-	| awk '{print "local-zone: \""$2"\" always_nxdomain"}' > $TEMPFILE.unbound
+	| awk 'NF {print "local-zone: \""$1".\" always_nxdomain"}' > $TEMPFILE.unbound
 
-# Install
+# Install, making sure the new list works bofore reloading
 cp $TEMPFILE.unbound $TARGET
+unbound-checkconf
 systemctl reload unbound
